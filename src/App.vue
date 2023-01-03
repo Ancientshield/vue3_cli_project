c9280891a292d6cc743e306c7eb09122ed2968dd
Ancientshield <ancientsword520@gmail.com>
2023-01-03 18:01:29 +0800

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-	<demoOne />
+	<button @click="isShowDemo = !isShowDemo">切換隱藏/顯示</button>
+	<demoOne v-if="isShowDemo" />
 </template>
 
 <script>
 	// import 元件不用 curly brackets❗️
-	import demoOne from './components/demoOne.vue';
 	// import { demoOne } from './components/demoOne.vue'; ...❌
+	import demoOne from './components/demoOne.vue';
+	import { ref } from 'vue';
 	export default {
 		name: 'app',
 		// 註冊元件
 		components: { demoOne },
+		setup() {
+			let isShowDemo = ref(true);
+			return {
+				isShowDemo,
+			};
+		},
 	};
 </script>
 
--- src/components/demoOne.vue
+++ src/components/demoOne.vue
@@ -1,20 +1,95 @@
 <template>
 	<h2>當前求和為：{{ sum }}</h2>
-	<br />
 	<button @click="sum++">點我+1</button>
 </template>
 
 <script>
-	import { ref } from 'vue';
+	import {
+		ref,
+		onBeforeMount,
+		onMounted,
+		onBeforeUpdate,
+		onUpdated,
+		onBeforeUnmount,
+		onUnmounted,
+	} from 'vue';
 	export default {
 		name: 'demoOne',
 		setup() {
+			console.log('---setup---');
 			// 資料
 			let sum = ref(0);
+
+			// ❗️通過【組合式API】（Composition API）的形式去使用生命週期鉤子
+			/*
+			beforeCreate() --> setup() 取代
+			created() -------> setup() 取代
+			beforeMount() ---> onBeforeMount()
+			mounted() -------> onMounted()
+			beforeUpdate() --> onBeforeUpdate()
+			updated() -------> onUpdated()
+			beforeUnmount() -> onBeforeUnmount()
+			unmounted() -----> onUnmounted()
+			*/
+			onBeforeMount(() => {
+				console.log('---onBeforeMount---');
+			});
+			onMounted(() => {
+				console.log('---onMounted---');
+			});
+			onBeforeUpdate(() => {
+				console.log('---onBeforeUpdate---');
+			});
+			onUpdated(() => {
+				console.log('---onUpdated---');
+			});
+			onBeforeUnmount(() => {
+				console.log('---onBeforeUnmount---');
+			});
+			onUnmounted(() => {
+				console.log('---onUnmounted---');
+			});
 			return {
 				sum,
 			};
 		},
+		// 透過配置項的形式使用生命週期鉤子
+		/*
+		beforeCreate() {
+			console.log('---beforeCreate---');
+		},
+		created() {
+			console.log('---created---');
+		},
+		beforeMount() {
+			console.log('---beforeMount---');
+		},
+		mounted() {
+			console.log('---mounted---');
+		},
+		beforeUpdate() {
+			console.log('---beforeUpdate---');
+		},
+		updated() {
+			console.log('---updated---');
+		},
+		beforeUnmount() {
+			console.log('---beforeUnmount---');
+		},
+		unmounted() {
+			console.log('---unmounted---');
+		},
+		*/
+
+		// 已失效
+		/* 
+		beforeDestroy(){
+			console.log('---beforeDestroy---');
+		},
+		destroyed(){
+			console.log('---destroyed---');
+		}
+		*/
 	};
 </script>
 
