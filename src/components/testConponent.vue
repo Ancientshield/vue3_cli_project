0ac8fa87a1c8d0255dcd0153a987e21e6e1655ba
Ancientshield <ancientsword520@gmail.com>
2023-01-03 20:43:15 +0800

    self defined hooks

--- src/components/testConponent.vue
+++ src/components/testConponent.vue
@@ -1,26 +1,15 @@
 <template>
-	<h2>當前求和為：{{ sum }}</h2>
-	<button @click="sum++">點我+1</button>
-	<hr />
+	<h2>我是testConponent元件</h2>
 	<h2>當前點擊滑鼠的座標 x：{{ point.x }}，y：{{ point.y }}</h2>
 </template>
-
 <script>
-	import { ref } from 'vue';
 	import usePoint from '../hooks/usePoint';
 	export default {
-		name: 'demoOne',
+		name: 'testConponent',
 		setup() {
-			// 資料
-			let sum = ref(0);
 			let point = usePoint();
 
-			return {
-				sum,
-				point,
-			};
+			return { point };
 		},
 	};
 </script>
-
-<style></style>
