96876a9ed8f3ca183bd8ff88306408d4e6e17d0e
Ancientshield <ancientsword520@gmail.com>
2023-01-03 21:36:21 +0800

    init 13

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
 	<button @click="isShowDemo = !isShowDemo">切換隱藏/顯示</button>
 	<demoOne v-if="isShowDemo" />
-	<hr />
-	<testConponent />
 </template>
 
 <script>
 	// import 元件不用 curly brackets❗️
 	// import { demoOne } from './components/demoOne.vue'; ...❌
 	import demoOne from './components/demoOne.vue';
-	import testConponent from './components/testConponent.vue';
 	import { ref } from 'vue';
 	export default {
 		name: 'app',
 		// 註冊元件
-		components: { demoOne, testConponent },
+		components: { demoOne },
 		setup() {
 			let isShowDemo = ref(true);
 			return {
--- src/components/demoOne.vue
+++ src/components/demoOne.vue
@@ -1,23 +1,29 @@
 <template>
-	<h2>當前求和為：{{ sum }}</h2>
-	<button @click="sum++">點我+1</button>
-	<hr />
-	<h2>當前點擊滑鼠的座標 x：{{ point.x }}，y：{{ point.y }}</h2>
+	<h2>姓名：{{ person.name }}</h2>
+	<h2>年齡：{{ person.age }}</h2>
+	<h2>薪資：{{ person.job.j1.salary }}K</h2>
+	<button @click="person.name += '~'">修改姓名</button>
+	<button @click="person.age++">增加年齡</button>
+	<button @click="person.job.j1.salary++">加薪</button>
 </template>
 
 <script>
-	import { ref } from 'vue';
-	import usePoint from '../hooks/usePoint';
+	import { reactive } from 'vue';
 	export default {
 		name: 'demoOne',
 		setup() {
-			// 資料
-			let sum = ref(0);
-			let point = usePoint();
+			let person = reactive({
+				name: 'Jack',
+				age: 18,
+				job: {
+					j1: {
+						salary: 70,
+					},
+				},
+			});
 
 			return {
-				sum,
-				point,
+				person,
 			};
 		},
 	};
