ec305d20a9953250f9cfddec4841d40802bddb6f
Ancientshield <ancientsword520@gmail.com>
2023-01-04 11:04:59 +0800

    shallowRef shallowReactive

---
+++ 14_shallowRef_shallowReactive/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<button @click="isShowDemo = !isShowDemo">切換隱藏/顯示</button>
+	<demoOne v-if="isShowDemo" />
+</template>
+
+<script>
+	// import 元件不用 curly brackets❗️
+	// import { demoOne } from './components/demoOne.vue'; ...❌
+	import demoOne from './components/demoOne.vue';
+	import { ref } from 'vue';
+	export default {
+		name: 'app',
+		// 註冊元件
+		components: { demoOne },
+		setup() {
+			let isShowDemo = ref(true);
+			return {
+				isShowDemo,
+			};
+		},
+	};
+</script>
+
+<style></style>
