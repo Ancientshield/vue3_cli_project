5421066e71fed6f5dc9ae0e5da81d3d057e509fe
Ancientshield <ancientsword520@gmail.com>
2023-01-04 13:46:15 +0800

    provide jnject

---
+++ 18_provide&inject/components/childComponent.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="child">
+		<h3>我是Child子元件</h3>
+		<sonComponent />
+	</div>
+</template>
+
+<script>
+	import sonComponent from './sonComponent.vue';
+	export default {
+		name: 'childComponent',
+		// 要記得註冊元件，且值是物件
+		components: { sonComponent },
+	};
+</script>
+
+<style>
+	.child {
+		background-color: skyblue;
+		padding: 10px;
+	}
+</style>
---
+++ 18_provide&inject/components/sonComponent.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="son">
+		<h3>我是Son孫元件，inject--{{ car.name }}--{{ car.price }}</h3>
+	</div>
+</template>
+
+<script>
+	import { inject } from 'vue';
+	export default {
+		name: 'sonComponent',
+		setup() {
+			let car = inject('car');
+			return { car };
+		},
+	};
+</script>
+
+<style>
+	.son {
+		background-color: orange;
+		padding: 10px;
+	}
+</style>
